fe437ad98cab7a46570b934ab9bad4f9e65cdcde
sadavar <sadavar@umich.edu>
2023-09-12 13:24:09 -0400

    iterates through dates for input

--- src/Import.js
+++ src/Import.js
@@ -13,35 +13,63 @@ export default function Import() {
 
     async function handleGenerate() {
         console.log("generating");
+        if (username == '') return;
         getPGNs();
     }
 
     async function getPGNs() {
         console.log("getting pgns of: " + username);
+        let date = new Date('January 1, 2023 06:00:00');
+        let month = ("0" + (date.getMonth() + 1)).slice(-2);
+        console.log(month);
 
-        const url = new URL('https://chess-trainer-backend-sadavar-6c10d1a552e9.herokuapp.com/getPGNs');
-        url.searchParams.append('username', username);
-        const res = await fetch(url);
-        pgns = await res.json();
-        setPGNs(pgns);
 
-        getTactics();
+
+        const chess_url = new URL('https://api.chess.com/pub/player/' + username + '/games/2023/');
+        // const chess_url1 = new URL('https://api.chess.com/pub/player/sadavar/games/2023/04');
+        var data_retreived = false;
+        var pgns;
+        while (!data_retreived) {
+            if (month == 13) {
+                console.log("no games found");
+                return;
+            }
+            try {
+                const res = await fetch(chess_url + month);
+                pgns = await res.json();
+                console.log("pgns");
+                console.log(pgns);
+                if (pgns.games.length == 0) {
+                    console.log("no games found");
+                    date.setMonth(date.getMonth() + 1);
+                } else {
+                    data_retreived = true;
+                }
+            }
+            catch (error) {
+                console.log("error");
+                console.log(error);
+                month++;
+                continue;
+            }
+        }
+        getTactics(pgns);
     };
 
-    async function getTactics() {
+    async function getTactics(pgns) {
         let pgn = pgns.games[0].pgn;
         console.log("getting tactics of: " + pgn);
 
         const url = new URL('https://chess-trainer-python-b932ead51c12.herokuapp.com/getTactics');
-        const url2 = new URL('https://chess-trainer-backend-sadavar-6c10d1a552e9.herokuapp.com/getTactics');
-        const url3 = new URL('http://127.0.0.1:5000/getTactics');
+        // const url2 = new URL('https://chess-trainer-backend-sadavar-6c10d1a552e9.herokuapp.com/getTactics');
+        // const url3 = new URL('http://127.0.0.1:5000/getTactics');
 
         url.searchParams.append('pgns', pgn);
         const res = await fetch(url);
-        const data = res.json();
-        setTactics(await data);
-        console.log("tactics found: " + await data);
-        await data.then(setTacticsLoaded(true)).then(console.log("tactics loaded!"));
+        const tactics = res.json();
+        setTactics(await tactics);
+        console.log("tactics found: " + await tactics);
+        await tactics.then(setTacticsLoaded(true)).then(console.log("tactics loaded!"));
 
     }
 
