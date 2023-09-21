b6459ccd2f841ba69850a612e157ebcf2aeae6c8
sadavar <sadavar@umich.edu>
2023-09-20 20:21:41 -0400

    hide console.logs in prod

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import { AppContext } from './AppContext';
 function App() {
   const [counter, setCounter] = useState(0);
 
+  // replace console.* for disable log on production
+  if (process.env.NODE_ENV === 'production') {
+    console.log = () => { }
+    console.error = () => { }
+    console.debug = () => { }
+  }
+
   return (
     <>
       <AppContext.Provider value={{ counter, setCounter }}>
