b1eae178894b1c91fa052329881c65f2318dbd7b
Varun Sadasivam <sadavar05@gmail.com>
2023-12-13 16:36:02 -0500

    chart

--- src/Import.js
+++ src/Import.js
@@ -164,8 +164,8 @@ export default function Import() {
     async function getTactics(pgn) {
         console.log("getting tactics of: " + pgn);
 
-        // const url = new URL('https://chess-trainer-python-b932ead51c12.herokuapp.com/getTactics');
-        const url = new URL('http://127.0.0.1:5000/getTactics');
+        const url = new URL('https://chess-trainer-python-b932ead51c12.herokuapp.com/getTactics');
+        // const url = new URL('http://127.0.0.1:5000/getTactics');
         var response;
         // get tactics
         try {
